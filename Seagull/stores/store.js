bac744dff989a035cb036e40fb248808eec92d5d
Jimmy Chou <koudaix700@gmail.com>
2015-05-28 16:24:43 +0800

    add test code

--- Seagull/stores/store.js
+++ Seagull/stores/store.js
@@ -1,8 +1,12 @@
 define(['Core','AbstractStorage'], function(Core,AbstractStorage) {
 	var Awatar = {};
     Awatar.TEST = new Core.Class(AbstractStorage,{
-        propertys: function() {
+        initialize: function () {
+            this.key = 'TEST';
             this.lifeTime = '1D';
+        },
+        setCities: function (str) {
+            this.setAttr("city",str);
         }
     });
     Awatar.NAXT = new Core.Class(AbstractStorage,{
